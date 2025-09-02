377af5586438f494257850f7a8587e9d02a2baa8
Arun-2010 <arunbandari15@gmail.com>
2025-09-02 16:48:22 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+   return <div>
+    <input type="text" placeholder='amount of sol' />
+    <input type="text" placeholder='to address' />
+    <button>submit</button>
+
+  </div>
+  
+}
+
+export default App
